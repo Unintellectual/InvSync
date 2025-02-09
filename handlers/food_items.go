0db4c2667d2d6dd62cc2a5f078b6308126e61b7e
Noah Victoriano <noah.victoriano@cvsu.edu.ph>
2025-02-09 18:56:25 +0800

    made env file and seperated the files

---
+++ handlers/food_items.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/Unintellectual/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func GetFoodItems(c *gin.Context) {
+	rows, err := database.DB.Query("SELECT id, name, category, quantity, price, expiry_date FROM food_items")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var foodItems []map[string]interface{}
+	for rows.Next() {
+		var id int
+		var name, category string
+		var quantity int
+		var price float64
+		var expiryDate *string
+		err := rows.Scan(&id, &name, &category, &quantity, &price, &expiryDate)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		foodItem := gin.H{
+			"id":          id,
+			"name":        name,
+			"category":    category,
+			"quantity":    quantity,
+			"price":       price,
+			"expiry_date": expiryDate,
+		}
+		foodItems = append(foodItems, foodItem)
+	}
+	c.JSON(http.StatusOK, foodItems)
+}
